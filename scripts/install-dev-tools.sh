b37992262fc3f63787492500fd0280a51d5535c5
Igor Bari <mucsi96@gmail.com>
2023-03-06 07:41:10 +0000

    create scripts folder

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pip install -r requirements.txt
 ansible-galaxy install -r galaxy-requirements.yaml
+rm -rf lib
+libs=https://raw.githubusercontent.com/mucsi96/ansible-roles/main/lib
+wget $libs/ansible_utils.py --directory-prefix lib
