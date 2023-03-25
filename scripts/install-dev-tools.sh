61a8edc961cf34175f5fbb63c151f3b2e348146d
Igor Bari <mucsi96@gmail.com>
2023-03-25 11:15:48 +0000

    use kubetools wheel

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
 ansible-galaxy install -r galaxy-requirements.yaml
-rm -rf lib
-libs=https://raw.githubusercontent.com/mucsi96/ansible-roles/main/lib
-wget $libs/ansible_utils.py --directory-prefix lib
